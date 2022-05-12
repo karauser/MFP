0147adcd75622df3e18a48676e9cba526e503ffd
Sergey <60179230+karauser@users.noreply.github.com>
2022-05-12 22:52:32 +0300

    Update PlaceModel.swift

--- MFP/Model/PlaceModel.swift
+++ MFP/Model/PlaceModel.swift
@@ -10,19 +10,20 @@ import RealmSwift
 
 class Place: Object {
     
-    @objc dynamic var name = ""
+    @objc dynamic var name   = ""
+    @objc dynamic var date   = Date()
+    @objc dynamic var rating = 0.0
     @objc dynamic var location: String?
     @objc dynamic var type: String?
     @objc dynamic var imageData: Data?
-    @objc dynamic var date = Date()
-    @objc dynamic var rating = 0.0
+   
     
     convenience init(name: String, location: String?, type: String?, imageData: Data?, rating: Double) {
         self.init()
-        self.name = name
-        self.location = location
-        self.type = type
+        self.name      = name
+        self.location  = location
+        self.type      = type
         self.imageData = imageData
-        self.rating = rating
+        self.rating.   = rating
     }
 }
